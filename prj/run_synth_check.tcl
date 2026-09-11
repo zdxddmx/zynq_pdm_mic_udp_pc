29ed8c0721df01995694642e95628d65c1f1a00c
zdxddmx <206847232+zdxddmx@users.noreply.github.com>
2026-09-12 00:22:01 +0800

    工程文件改用相对路径，移除本机绝对路径

--- prj/run_synth_check.tcl
+++ prj/run_synth_check.tcl
@@ -1,10 +1,3 @@
-open_project C:/Users/29229/Desktop/ZYNQ_7020_FPGA/47_pdm_mic_udp_pc/prj/ad_udp_pc.xpr
-reset_run synth_1
-launch_runs synth_1 -jobs 4
-wait_on_run synth_1
-set synth_status [get_property STATUS [get_runs synth_1]]
-puts "SYNTH_STATUS=$synth_status"
-if {[string first "Complete" $synth_status] < 0} {
-    exit 1
-}
-exit 0
+# 在仓库根目录、或在 Vivado Tcl Console 中 source 本脚本执行
+set prj_dir [file dirname [file dirname [info script]]]
+open_project [file join $prj_dir prj ad_udp_pc.xpr]
